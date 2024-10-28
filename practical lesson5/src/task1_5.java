e8538d7ed1091cbf9c5c4072e08c74295917eda6
knockdown99 <yarmolenkovlad012@gmail.com>
2024-10-28 18:35:14 +0200

    new file:   practical lesson5/README.md 	new file:   practical lesson5/src/task1_5.java 	new file:   practical lesson5/src/task2_5.java

---
+++ practical lesson5/src/task1_5.java
@@ -0,0 +1,31 @@
+public class task1_5 {
+    public static void sortDescending(byte[] array) {
+        int n = array.length;
+
+        // Один за одним перебираємо всі елементи масиву
+        for (int i = 0; i < n - 1; i++) {
+            // Знаходимо індекс максимального елемента в невідсортованій частині
+            int maxIndex = i;
+            for (int j = i + 1; j < n; j++) { //починаємо пошук максимального елемента з елемента, що йде відразу після поточного i
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            // Міняємо місцями максимальний елемент з елементом на початку невідсортованої частини
+            byte temp = array[maxIndex]; // створюємо тимчасову змінну
+            array[maxIndex] = array[i]; // переміщуємо поточний елемент на місце найбільшого
+            array[i] = temp; // передаємо значення найбільшого елементу індексу початкового
+        }
+    }
+
+    public static void main(String[] args) {
+        byte[] numbers = {3, 7, 4, 9, 5, 2, 6, 1, 2, 1, 0, 11, 5};
+        sortDescending(numbers);
+
+        // Виводимо відсортований масив
+        for (byte number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
